277fea149593084bada8a0ef8b8ffe416d129c00
unknown <hamid.shahmohamadi@gmail.com>
2021-07-02 12:48:21 +0330

    00-4-11-1248

---
+++ layouts/partials/navigation.vue
@@ -0,0 +1,113 @@
+<template>
+  <header class="fixed-topx">
+    <nav class="navbar navbar-expand-lg navbar-dark align-items-center">
+      <a href="/" class="navbar-brand text-uppercase fw-500">DesignHouse</a>
+      <button
+        class="navbar-toggler mr-auto"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbar"
+        aria-controls="navbar"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav font-14 fw-300">
+          <li class="nav-item">
+            <a href="/designs" class="nav-link">Designs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" title="Designers">Designers</a>
+          </li>
+        </ul>
+        <div class="header-search">
+          <form action method>
+            <div class="form-group">
+              <input
+                type="text"
+                autocomplete="OFF"
+                name="search"
+                class="form-control text-white font-14 fw-300"
+                placeholder="Search and hit enter..."
+              />
+              <div class="search-icon text-white">
+                <i class="fas fa-search"></i>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="upload-shot white-path font-14 fw-500 text-uppercase mr-auto">
+          <a href="/upload" class="primary-bg-color text-white">
+            <i class="fas fa-cloud-upload-alt"></i> Upload
+          </a>
+        </div>
+      </div>
+
+
+      
+        <ul class="author-page white-path">
+          <!-- Profile Dropdown -->
+          <li class="dropdown">
+            <a
+              href="#"
+              class="dropdown-toggle text-white"
+              id="userDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <img class="user-thumb" src="~/assets/images/profile.png" />
+              <div class="usr-info">
+                <span class="user-name font-14 fw-500">
+                  john doe
+                </span>
+                <span class="user-deg font-10 fw-300">
+                  UI Designer
+                </span>
+                <span class="down-chevron">
+                  <i class="fa fa-angle-down"></i>
+                </span>
+              </div>
+            </a>
+            <div class="dropdown-menu user-dropdown font-14 fw-500" aria-labelledby="userDropdown">
+              <div class="dropdown-title-group font-12 fw-500">
+                <span class="dropdown-title text-uppercase">Your Account</span>
+              </div>
+              <nuxt-link to="/settings/dashboard" class="dropdown-item mt-28">
+                <i class="fas fa-tachometer-alt"></i>
+                Dashboard
+              </nuxt-link>
+              <a class="dropdown-item" href="#" title="Profile">
+                <i class="fa fa-user"></i>
+                Profile
+              </a>
+              <a class="dropdown-item" href="#" title="Setting">
+                <i class="fa fa-cogs"></i>
+                Setting
+              </a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#" >
+                <i class="fa fa-lock"></i>
+                Sign Out
+              </a>
+            </div>
+          </li>
+          <!-- End Profile Dropdown -->
+        </ul>
+      
+     
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  methods: {
+  }
+};
+</script>
+
+<style></style>
---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+export default {
+  publicRuntimeConfig: {
+    baseUrl:"http://localhost:3000",
+    apiUrl:"http://design.test/api"
+  },
+  privateRuntimeConfig: {},
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'design-nuxt',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script:[
+      {
+        src:'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js'
+      }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/scss/main.scss'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    baseUrl:"http://design.test/api"
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
